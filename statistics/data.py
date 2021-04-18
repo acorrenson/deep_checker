46ee4e1f6bd299a2a99ef503e07ca1664724c9e9
Manon Sourisseau <manon.sourisseau@ens-rennes.fr>
2021-04-18 18:59:43 +0200

    Code plus propre

--- statistics/data.py
+++ statistics/data.py
@@ -5,11 +5,11 @@ data = []
 with open("example.csv") as fichier :
     for game in fichier :
         coups = game.split()
-        c_couple = []
+        liste_couples = []
         for i in range (0,len(coups) - 1,2) :
             couple = (int(coups[i],16),int(coups[i+1],16))
-            c_couple.append(couple)
-        data.append(c_couple)
+            liste_couples.append(couple)
+        data.append(liste_couples)
 
 
     
