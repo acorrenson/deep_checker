f4caa94ab05925764145ee0bb379059859969a4d
Manon Sourisseau <manon.sourisseau@ens-rennes.fr>
2021-04-18 18:58:40 +0200

    ajout d'un parser pour utiliser les données sous python

--- statistics/data.py
+++ statistics/data.py
@@ -1,3 +1,16 @@
 import pandas
+    
 
+data = []
+with open("example.csv") as fichier :
+    for game in fichier :
+        coups = game.split()
+        c_couple = []
+        for i in range (0,len(coups) - 1,2) :
+            couple = (int(coups[i],16),int(coups[i+1],16))
+            c_couple.append(couple)
+        data.append(c_couple)
+
+
+    
 # Here read the data
