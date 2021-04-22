97ba0053b2e7e3f62767f9fc9a51e8e1c350ed00
Igor Martayan <igor@martayan.org>
2021-04-22 10:55:41 +0200

    Generic function to ask program

--- simulator/game.h
+++ simulator/game.h
@@ -14,9 +14,14 @@ vector *generate_moves(vector *player_choice, unsigned player,
 vector *generate_takes(vector *player_choice, vector *opponent_choice,
                        unsigned player, unsigned opponent);
 
-unsigned ask_knn(vector *moves);
+unsigned ask_program(vector *moves, char *cmd);
+
+void play_program(unsigned *player, unsigned *opponent, int direction,
+                  char *cmd);
 
 void play_knn(unsigned *player, unsigned *opponent, int direction);
+
+void play_mlp(unsigned *player, unsigned *opponent, int direction);
 
 void play_match(strategy strat1, strategy strat2, vector *boards,
                 int show_board);
