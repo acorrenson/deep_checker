b471a61e2ea25a275e1855139cc02243591ea6cf
Igor Martayan <igor@martayan.org>
2021-04-22 14:49:34 +0200

    Update train_mlp.py

--- statistics/train_mlp.py
+++ statistics/train_mlp.py
@@ -1,12 +1,20 @@
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
 import joblib
+import sys
 
-X, Y = joblib.load("scores.save", "r")
+scores = "scores.save"
+if len(sys.argv) > 1:
+    scores = sys.argv[1]
+out = "mlp.save"
+if len(sys.argv) > 2:
+    out = sys.argv[2]
+
+X, Y = joblib.load(scores, "r")
 X_train, X_test, y_train, y_test = train_test_split(X, Y.ravel())
 
 mlp = MLPRegressor(
-    hidden_layer_sizes=(64, 64, 32, 16), activation="relu", solver="adam"
+    hidden_layer_sizes=(64, 64, 32, 16), activation="relu", solver="adam", max_iter=500
 )
 mlp.fit(X_train, y_train)
 
@@ -14,4 +22,4 @@ print(mlp.predict(X_test[:4]))
 print(y_test[:4])
 print(mlp.score(X_test, y_test))
 
-joblib.dump(mlp, "mlp.save")
+joblib.dump(mlp, out)
